ca5813c4beb073abc802bcf4e0d4706221e79184
gui-ferreiraa <11212100671@alunos.umc.br>
2025-04-13 23:11:17 -0300

    feat: global table

---
+++ web/src/app/core/components/table/table.component.ts
@@ -0,0 +1,19 @@
+import { Component, EventEmitter, input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-table',
+  imports: [],
+  templateUrl: './table.component.html',
+})
+export class TableComponent {
+  title = input.required<string>();
+  subtitle = input.required<string>();
+  theads = input.required<string[]>();
+  buttonTitle = input.required<string>();
+  @Output() buttonClicked = new EventEmitter<void>();
+
+  onClickButton() {
+    this.buttonClicked.emit();
+  }
+
+}
