d4da602f88797843efb93e30ec71463fc6589ee5
guilhermeferreira0 <guilherme.fsilva23@gmail.com>
2025-05-02 21:48:37 -0300

    refactor: environments web

---
+++ web/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  API_BASE_URL: import.meta.env.NG_SUS_API_BASE_URL,
+  ASSISTANT_BASE_URL: import.meta.env.NG_SUS_ASSISTANT_BASE_URL,
+  appVersion: import.meta.env.NG_SUS_APP_VERSION
+};
